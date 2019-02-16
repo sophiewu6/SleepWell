54f51bf64b9c978e57c6837f7fa8e04813a92f79
Felicity Zhao <felicityzhao@dhcp-v102-173.mobile.uci.edu>
2019-02-16 10:27:24 -0800

    first commit

---
+++ app/src/main/java/com/example/felicityzhao/sleepwell/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.felicityzhao.sleepwell;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_music:
+                    mTextMessage.setText(R.string.title_music);
+                    return true;
+                case R.id.navigation_alarm:
+                    mTextMessage.setText(R.string.title_alarm);
+                    return true;
+                case R.id.navigation_analysis:
+                    mTextMessage.setText(R.string.title_analysis);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
